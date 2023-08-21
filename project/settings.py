f6df50fbe92415bba084f50563d26a5131143c6e
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-08-21 08:51:35 +0545

    enchnagement:installing and dumping required data#6

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tms'
+    'tms',
+    'djoser',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
